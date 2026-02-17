02a9c9a6e94a15c6e0c3cdafd60665caeef099b4
tharunkumr-op <tharunkumar1156@gmail.com>
2026-02-17 19:02:57 +0530

    account update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -2,33 +2,58 @@ from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 SECRET_KEY = 'dev-secret-change-me'
+
 DEBUG = True
+
 ALLOWED_HOSTS = ["*"]
 
+
 INSTALLED_APPS = [
+    # Django default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Required for django-allauth
+    'django.contrib.sites',
+
+    # Your apps
     'crispy_forms',
     'shop',
+
+    # Allauth apps
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
+
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # whitenoise should be near top
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    # Required for allauth
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
+
 ROOT_URLCONF = 'ecommerce.urls'
+
 
 TEMPLATES = [
     {
@@ -38,7 +63,10 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
+
+                # REQUIRED FOR ALLAUTH
                 'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -46,7 +74,9 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
+
 
 DATABASES = {
     'default': {
@@ -55,23 +85,54 @@ DATABASES = {
     }
 }
 
+
 AUTH_PASSWORD_VALIDATORS = []
+
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'Asia/Kolkata'
+
 USE_I18N = True
+
 USE_TZ = True
 
+
+# ================= STATIC FILES =================
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'shop' / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+
+# ================= MEDIA FILES =================
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# ================== ALLAUTH SETTINGS ==================
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_USERNAME_REQUIRED = False
+
+# IMPORTANT (otherwise allauth may block login)
+ACCOUNT_EMAIL_VERIFICATION = "none"
+
 
 # ================= UPI SETTINGS =================
 GPAY_UPI_ID = "tharunkumar2124@okhdfcbank"
